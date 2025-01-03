a3ff25ed077c803884d25ba5af27cf7ba9d90f95
pempek18 <pempek183@gmail.com>
2025-01-03 21:48:36 +0100

    expirience is almost finished working on cv generator

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
+import python_pdfkit as pdfkit
 
 app = Flask(__name__)
 
@@ -21,6 +22,31 @@ def services():
 @app.route('/production')
 def production():
     return render_template('production.html')
+
+@app.route('/generate_cv')
+def generate_cv():
+    # Render the template to HTML
+    html = render_template('experience.html')
+    
+    # Configure PDF options if needed
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8",
+    }
+    
+    # Generate PDF from HTML
+    pdf = pdfkit.from_string(html, False, options=options)
+    
+    # Create the response
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=cv.pdf'
+    
+    return response
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', debug=True) 
